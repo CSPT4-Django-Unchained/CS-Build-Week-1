d39119e41bcb55962d405855338db21699ce4416
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-01-24 00:41:30 +0800

    Add route to rooms data

--- adventure/urls.py
+++ adventure/urls.py
@@ -1,7 +1,14 @@
+from django.urls import include, path
 from django.conf.urls import url
+from rest_framework import routers
+from . import views
 from . import api
 
+router = routers.DefaultRouter()
+router.register(r'rooms', views.RoomViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
