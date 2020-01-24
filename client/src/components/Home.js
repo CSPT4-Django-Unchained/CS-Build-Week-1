ac816c9034ff236dfa9198ac2b6dd70d16ab1a8b
LD <LDinho@users.noreply.github.com>
2020-01-23 20:06:41 -0800

    add password type to input element; add login link from home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 const Home = () => {
 
@@ -6,8 +8,13 @@ const Home = () => {
     <div className="App">
       <header className="App-header">
         <p>
-          django_unchained
+          django_unchained MUD
         </p>
+
+        <Link to='/login'>
+          <Button > Register/Login </Button>
+        </Link>
+
       </header>
     </div>
   );
